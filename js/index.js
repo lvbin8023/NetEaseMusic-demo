8915238f511f1c526916355cd38346cab65a58dd
lvbin <472245767@qq.com>
2019-01-22 00:09:32 +0800

    网页云音乐歌曲播放优化

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(function () {
-  $.get('../song.json', function (response) {
+  $.get('http://plggyay5q.bkt.clouddn.com/song.json', function (response) {
     let items = response;
     items.forEach(item => {
       let $li = $(`<li>
--- js/song.js
+++ js/song.js
@@ -2,7 +2,7 @@ $(function () {
   let urlLocation = window.location.search;
   let urlId = parseInt(urlLocation.match(/\bid=([^&]*)/)[1]);
 
-  $.get('../song.json').then(function (response) {
+  $.get('http://plggyay5q.bkt.clouddn.com/song.json').then(function (response) {
     //歌曲
     let songs = response;
     let song = songs.filter((s) => {
